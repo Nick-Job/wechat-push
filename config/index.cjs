d50035bf312fae104d062f7bb065c74b6241ed25
Nick-Job <83775021+Nick-Job@users.noreply.github.com>
2023-04-10 23:12:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -111,7 +111,7 @@ const USER_CONFIG = {
       name: 'test',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: '',
+      id: 'o1oGa5qtF_VkP9taz4JuI9x3xFXM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: 'Md0m3hDBVCgKMBD5Cy_4K1fOZZraPqU9zpXdG_-7ELk',
